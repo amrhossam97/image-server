0567cae284df10468991fee60f72539954df4b86
Ahmad Eltobshy <74884132+AhmadEltobshy@users.noreply.github.com>
2021-03-02 05:35:06 +0200

    deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ let routes = (app) => {
   router.post("/upload/:id", controller.upload);
   router.get("/files", controller.getListFiles);
   router.get("/files/:name", controller.download);
+  router.get("/", controller.Home);
 
   app.use(router);
 };
